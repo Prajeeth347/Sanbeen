3b55026bd9adbfab0d369068f09351b3e1f04440
Prajeeth347 <prajeeth347@gmail.com>
2022-07-09 10:28:07 +0530

    8th page

--- lib/screens/rentals_page.dart
+++ lib/screens/rentals_page.dart
@@ -25,7 +25,6 @@ class _rentals_pageState extends State<rentals_page> {
   ScrollController? _scrollController;
   bool lastStatus = true;
   double height = 200;
-
   void _scrollListener() {
     if (_isShrink != lastStatus) {
       setState(() {
@@ -166,104 +165,105 @@ class _rental_bodyState extends State<rental_body> {
   }
 
   var number;
+  List collectio = ["Hi","Hello","Hola"];
+  List furnishing = ["Hi","Hello","Hola"];
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(children: [
-        // SizedBox(height: 20),
-        Padding(
-          padding: EdgeInsets.fromLTRB(24, 15, 16, 8),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                "Recently Posted Properties",
-                style: TextStyle(
-                    color: Theme.of(context).primaryColor,
-                    fontSize: MediaQuery.of(context).size.width * 0.05),
-              ),
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => properties_20()));
-                },
-                child: Text("View all",
-                    style: TextStyle(
-                        color: Theme.of(context).primaryColor,
-                        fontSize: MediaQuery.of(context).size.width * 0.04,
-                        decoration: TextDecoration.underline)),
-              )
-            ],
-          ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+      // SizedBox(height: 20),
+      Padding(
+        padding: EdgeInsets.fromLTRB(16, 15, 16, 8),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "Recently Posted Properties",
+              style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                  fontSize: MediaQuery.of(context).size.width * 0.05),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => properties_20()));
+              },
+              child: Text("View all",
+                  style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontSize: MediaQuery.of(context).size.width * 0.04,
+                      decoration: TextDecoration.underline)),
+            )
+          ],
         ),
+      ),
+      if (_prop_20_loaded == false)
+        CircularProgressIndicator(color: Theme.of(context).primaryColor),
+      if (_prop_20_loaded == true)
         SizedBox(
-          height: MediaQuery.of(context).size.height * 0.01,
+          height: 200,
+          child: ListView.builder(
+              itemCount: property?.length,
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: ((context, index) {
+                if (property![index].propertyMode == "rent")
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) =>
+                              property_single_page(id: property![index].id)));
+                    },
+                    child: Column(
+                      children: [
+                        Padding(
+                            child: Stack(
+                              children: [
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(10),
+                                  child: Container(
+                                    width: 170,
+                                    height: 120,
+                                    child: Image.network(
+                                      property![index].images[0],
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                                Align(
+                                  alignment: Alignment.topRight,
+                                  child: Padding(
+                                    padding: const EdgeInsets.fromLTRB(
+                                        120, 0, 0, 0),
+                                    child: IconButton(
+                                        onPressed: () {},
+                                        icon: Icon(
+                                          Icons.favorite_outline,
+                                          size: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
+                                              0.065,
+                                        )),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            padding: EdgeInsets.fromLTRB(10, 8, 10, 12)),
+                        Text(property![index].name,
+                            style: TextStyle(
+                                color: Theme.of(context).primaryColor,
+                                fontSize: MediaQuery.of(context).size.width *
+                                    0.04)),
+                      ],
+                    ),
+                  );
+                return Container();
+              })),
         ),
-        if (_prop_20_loaded == false)
-          CircularProgressIndicator(color: Theme.of(context).primaryColor),
-        if (_prop_20_loaded == true)
-          SizedBox(
-            height: 200,
-            child: ListView.builder(
-                itemCount: property?.length,
-                shrinkWrap: true,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: ((context, index) {
-                  if (property![index].propertyMode == "rent")
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) =>
-                                property_single_page(id: property![index].id)));
-                      },
-                      child: Column(
-                        children: [
-                          Padding(
-                              child: Stack(
-                                children: [
-                                  ClipRRect(
-                                    borderRadius: BorderRadius.circular(10),
-                                    child: Container(
-                                      width: 170,
-                                      height: 120,
-                                      child: Image.network(
-                                        property![index].images[0],
-                                        fit: BoxFit.cover,
-                                      ),
-                                    ),
-                                  ),
-                                  Align(
-                                    alignment: Alignment.topRight,
-                                    child: Padding(
-                                      padding: const EdgeInsets.fromLTRB(
-                                          120, 0, 0, 0),
-                                      child: IconButton(
-                                          onPressed: () {},
-                                          icon: Icon(
-                                            Icons.favorite_outline,
-                                            size: MediaQuery.of(context)
-                                                    .size
-                                                    .width *
-                                                0.065,
-                                          )),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              padding: EdgeInsets.fromLTRB(10, 8, 10, 12)),
-                          Text(property![index].name,
-                              style: TextStyle(
-                                  color: Theme.of(context).primaryColor,
-                                  fontSize: MediaQuery.of(context).size.width *
-                                      0.04)),
-                        ],
-                      ),
-                    );
-                  return Container();
-                })),
-          ),
-        Container(
+      Center(
+        child: Container(
           width: MediaQuery.of(context).size.width * 0.9,
           height: MediaQuery.of(context).size.height * 0.2,
           color: Theme.of(context).primaryColor,
@@ -338,173 +338,61 @@ class _rental_bodyState extends State<rental_body> {
             ],
           ),
         ),
-        SizedBox(
-          height: MediaQuery.of(context).size.height * 0.02,
-        ),
-        Text(
+      ),
+      SizedBox(
+        height: MediaQuery.of(context).size.height * 0.02,
+      ),
+      Padding(
+        padding: const EdgeInsets.fromLTRB(16,8,8,8),
+        child: Text(
           "Rental Collections",
           style: TextStyle(
               color: Theme.of(context).primaryColor,
               fontSize: MediaQuery.of(context).size.width * 0.05),
           textAlign: TextAlign.start,
         ),
-        Container(
-          height: MediaQuery.of(context).size.height * 0.37,
-          width: MediaQuery.of(context).size.width * 0.9,
-          color: Theme.of(context).primaryColor,
-          child: Padding(
-            padding: EdgeInsets.all(10),
-            child: Column(children: [
-              Text(
-                'Featured Dealers',
-                style: TextStyle(
-                    color: Theme.of(context).backgroundColor,
-                    fontSize: MediaQuery.of(context).size.width * 0.05,
-                    fontWeight: FontWeight.w600),
-                textAlign: TextAlign.left,
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.01,
-              ),
-              ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Container(
-                  height: MediaQuery.of(context).size.height * 0.3,
-                  width: MediaQuery.of(context).size.width * 0.4,
-                  color: Theme.of(context).backgroundColor,
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height * 0.01,
-                      ),
-                      CircleAvatar(
-                        radius: MediaQuery.of(context).size.width * 0.06,
-                        backgroundImage: AssetImage('assets/images/bill.png'),
-                      ),
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height * 0.01,
-                      ),
-                      Text('Sid Mathews',
-                          style: TextStyle(
-                              color: Theme.of(context).hintColor,
-                              fontSize:
-                                  MediaQuery.of(context).size.width * 0.04)),
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height * 0.005,
-                      ),
-                      Text('5 Buyers this week',
-                          style: TextStyle(
-                              color: Theme.of(context).hintColor,
-                              fontSize:
-                                  MediaQuery.of(context).size.width * 0.03)),
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height * 0.005,
-                      ),
-                      Text('Member Since Mar,2022',
-                          style: TextStyle(
-                              color: Theme.of(context).hintColor,
-                              fontSize:
-                                  MediaQuery.of(context).size.width * 0.025)),
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height * 0.005,
-                      ),
-                      Center(
-                        child: Row(
-                          children: [
-                            SizedBox(
-                              width: MediaQuery.of(context).size.width * 0.05,
-                            ),
-                            Icon(
-                              Icons.star_rounded,
-                              color: Theme.of(context).primaryColor,
-                            ),
-                            Icon(
-                              Icons.star_rounded,
-                              color: Theme.of(context).primaryColor,
-                            ),
-                            Icon(
-                              Icons.star_rounded,
-                              color: Theme.of(context).primaryColor,
-                            ),
-                            Icon(
-                              Icons.star_rounded,
-                              color: Theme.of(context).primaryColor,
-                            )
-                          ],
-                        ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          IconButton(
-                              onPressed: () async {
-                                var whatsappURl_android =
-                                    "whatsapp://send?phone=" +
-                                        number +
-                                        "&text=Hello";
-                                var whatappURL_ios =
-                                    "https://wa.me/$number?text=${Uri.parse("Hello")}";
-                                if (Platform.isIOS) {
-                                  // for iOS phone only
-                                  if (await canLaunchUrl(
-                                      Uri(path: whatappURL_ios))) {
-                                    await launchUrl(Uri(path: whatappURL_ios));
-                                  } else {
-                                    ScaffoldMessenger.of(context).showSnackBar(
-                                        SnackBar(
-                                            content: new Text(
-                                                "whatsapp no installed")));
-                                  }
-                                } else {
-                                  if (await canLaunch(whatsappURl_android)) {
-                                    await launch(whatsappURl_android);
-                                  } else {
-                                    ScaffoldMessenger.of(context).showSnackBar(
-                                        SnackBar(
-                                            content: new Text(
-                                                "whatsapp no installed")));
-                                  }
-                                }
-                              },
-                              icon: Image.network(
-                                  'https://cdn-icons-png.flaticon.com/512/124/124034.png?w=740&t=st=1656751163~exp=1656751763~hmac=3e5cd586eb711d53069f6ec6c5f9fdbd67c451cca7915d8684aae3d1a53cee94')),
-                          Text('Whatsapp',
-                              style: TextStyle(
-                                  color: Theme.of(context).primaryColor,
-                                  fontSize:
-                                      MediaQuery.of(context).size.width * 0.03,
-                                  fontWeight: FontWeight.w500)),
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          IconButton(
-                              color: Theme.of(context).hintColor,
-                              onPressed: () {
-                                FlutterPhoneDirectCaller.callNumber(number);
-                              },
-                              icon: Icon(
-                                Icons.phone,
-                                color: Theme.of(context).primaryColor,
-                                size: 20,
-                              )),
-                          Text('Call Now',
-                              style: TextStyle(
-                                  color: Theme.of(context).primaryColor,
-                                  fontSize:
-                                      MediaQuery.of(context).size.width * 0.03,
-                                  fontWeight: FontWeight.w500))
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-              )
-            ]),
+      ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(16,8,8,8),
+          child: SizedBox(
+            height: 130,
+            child: ListView.builder(
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              itemCount: collectio.length,
+              itemBuilder: ((context,index){
+                return Text(collectio[index],style: TextStyle(color: Colors.white),);
+              }),
+            ),
           ),
         ),
-      ]),
-    );
+        SizedBox(
+        height: MediaQuery.of(context).size.height * 0.02,
+      ),
+      Padding(
+        padding: const EdgeInsets.fromLTRB(16,8,8,8),
+        child: Text(
+          "Homes by furnishing",
+          style: TextStyle(
+              color: Theme.of(context).primaryColor,
+              fontSize: MediaQuery.of(context).size.width * 0.05),
+          textAlign: TextAlign.start,
+        ),
+      ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(16,8,8,8),
+          child: SizedBox(
+            height: 150,
+            child: ListView.builder(
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              itemCount: furnishing.length,
+              itemBuilder: ((context,index){
+                return Text(furnishing[index],style: TextStyle(color: Colors.white),);
+              }),
+            ),
+          ),
+        ),
+    ]);
   }
 }
